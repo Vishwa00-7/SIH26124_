928e66842f18a5296d784467f10b5a8142c05786
Vishwa00-7 <vigneshvishwa2003@gmail.com>
2026-09-07 06:27:04 +0530

    Update exec.py

--- exec.py
+++ exec.py
@@ -4,7 +4,7 @@ commands = [
     "python mosquitto/broker.py",
     "cd server && python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload",
     "python main.py --source edge/assets/test2.mp4 --fps 10 --show-video --verbose --aspect-ratio 16:9 --crop-anchor center",
-    "cd frontend ; npm run dev"
+    "cd frontend && npm run dev"
 ]
 
 for command in commands:
